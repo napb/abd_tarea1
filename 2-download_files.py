cea7e03b5860c8135008fdc691d7ae5461a68e5e
Nicolas Pastor Bueno <nicolas@MacBook-Pro-de-Nicolas.local>
2022-06-21 13:10:31 -0400

    initital commmit

---
+++ 2-download_files.py
@@ -0,0 +1,34 @@
+'''
+Load function that's:
+ - make a request to a url list
+ - obtains the content from the request
+ - extract zip files
+ - filter all files that is not a .csv files and delete the rest
+'''
+
+import os
+import requests, zipfile, io
+
+l = [
+    "https://datosabiertos.mineduc.cl/wp-content/uploads/2021/12/Asistencia-Declarada-Diciembre-2019.zip",
+    "https://datosabiertos.mineduc.cl/wp-content/uploads/2021/12/Asistencia-Declarada-Noviembre-2019.zip",
+    "https://datosabiertos.mineduc.cl/wp-content/uploads/2021/12/Asistencia-Declarada-Octubre-2019.zip",
+]
+
+os.makedirs("down", exist_ok=True)
+
+for i in l:
+    r = requests.get(i)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall("down")
+    z.close()
+
+    print(i)
+
+
+dir_name = "down"
+test = os.listdir(dir_name)
+
+for item in test:
+    if not item.endswith(".csv"):
+        os.remove(os.path.join(dir_name, item))
