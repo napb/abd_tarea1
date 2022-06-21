46bc4bb9a2236395ff2d3ace4e12352f101c08d2
Nicolas Pastor Bueno <nicolas@MacBook-Pro-de-Nicolas.local>
2022-06-21 15:27:53 -0400

    20220621_1527

--- 2/2-load-into-bq.py
+++ 2/2-load-into-bq.py
@@ -3,6 +3,8 @@ import os
 
 for f in os.listdir():
     if f.endswith(".csv"):
-        bashCommand = 'bq load --null_marker=null --skip_leading_rows=1 --source_format=CSV --field_delimiter=";" --allow_quoted_newlines=TRUE --allow_jagged_rows=TRUE data_chile.asistencia ./down/' + f
+        print(f)
+        bashCommand = "bq load --null_marker=null --skip_leading_rows=1 --source_format=CSV --field_delimiter=; --allow_quoted_newlines=TRUE --allow_jagged_rows=TRUE data_chile.asistencia ./" + f
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        process.communicate()
+        o, _ = process.communicate()
+        print(o)
