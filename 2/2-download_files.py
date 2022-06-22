0781acf414ecddbc045cc091593b16e19c8c3569
Nicolas Pastor Bueno <nicolas@MacBook-Pro-de-Nicolas.local>
2022-06-22 19:45:21 -0400

    20220622_1945

--- 2/2-download_files.py
+++ 2/2-download_files.py
@@ -9,24 +9,24 @@ Load function that's:
 import os
 import requests, zipfile, io
 
+dir_name = "../downloads"
+
 l = [
     "https://datosabiertos.mineduc.cl/wp-content/uploads/2021/12/Asistencia-Declarada-Diciembre-2019.zip",
     "https://datosabiertos.mineduc.cl/wp-content/uploads/2021/12/Asistencia-Declarada-Noviembre-2019.zip",
     "https://datosabiertos.mineduc.cl/wp-content/uploads/2021/12/Asistencia-Declarada-Octubre-2019.zip",
 ]
 
-os.makedirs("down", exist_ok=True)
+os.makedirs(dir_name, exist_ok=True)
 
 for i in l:
     r = requests.get(i)
     z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall("down")
+    z.extractall(dir_name)
     z.close()
-
     print(i)
 
 
-dir_name = "down"
 test = os.listdir(dir_name)
 
 for item in test:
