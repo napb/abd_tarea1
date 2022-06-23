d4248897a49252264ecbbe1e96c7bd50edc926fc
Nicolas Pastor Bueno <nicolas@MacBook-Pro-de-Nicolas.local>
2022-06-22 20:12:14 -0400

    20220622_2012

--- 2/2-load-into-bq.py
+++ 2/2-load-into-bq.py
@@ -1,10 +1,13 @@
 import subprocess
 import os
 
+print("Descarga de archivos")
 for f in os.listdir("../downloads"):
     if f.endswith(".csv"):
         print(f)
-        bashCommand = "bq load --null_marker=null --skip_leading_rows=1 --source_format=CSV --field_delimiter=; --allow_quoted_newlines=TRUE --allow_jagged_rows=TRUE data_chile.asistencia ./downloads/" + f
+        bashCommand = "bq load --null_marker=null --skip_leading_rows=1 --source_format=CSV --field_delimiter=; --allow_quoted_newlines=TRUE --allow_jagged_rows=TRUE data_chile.asistencia ../downloads/" + f
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         o, _ = process.communicate()
         print(o)
+
+print("------------------------------")
