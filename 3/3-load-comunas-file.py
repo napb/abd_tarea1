2855fcb7075a569ba171cb3827c76d8ada026083
Nicolas Pastor Bueno <nicolas@MacBook-Pro-de-Nicolas.local>
2022-06-22 20:17:38 -0400

    20220622_2017

--- 3/3-load-comunas-file.py
+++ 3/3-load-comunas-file.py
@@ -5,13 +5,13 @@ import subprocess
 url = "https://raw.githubusercontent.com/napb/abd_tarea1/master/3/comunas.csv"
 response = requests.get(url)
 
-with open('out.csv', 'w') as f:
+with open('../out.csv', 'w') as f:
     writer = csv.writer(f)
     for line in response.iter_lines():
         writer.writerow(line.decode('utf-8').split(','))
 
 
-bashCommand = "bq load --autodetect --source_format=CSV --field_delimiter=, data_chile.info_comunas ./out.csv"
+bashCommand = "bq load --autodetect --source_format=CSV --field_delimiter=, data_chile.info_comunas ../out.csv"
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 o, _ = process.communicate()
 print(o)
